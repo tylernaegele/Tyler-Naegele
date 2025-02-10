9b08459a25354010e255322741ba8b9c74716729
tylernaegele <nt79879@mail.harpercollege.edu>
2025-02-09 21:26:07 -0600

    Add files via upload

---
+++ M4P1.py
@@ -0,0 +1,9 @@
+# input phase
+exam_one = float(input("Enter score for exam one: "))
+exam_two = float(input("Enter score for exam two: "))
+
+# processing phase
+total = exam_one * .6 + exam_two * .4
+
+# output phase
+print("Total score is: ", total)
