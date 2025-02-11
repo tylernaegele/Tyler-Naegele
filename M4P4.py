d3dbe9da4f896726978086b603d5d4f5d61df34d
tylernaegele <nt79879@mail.harpercollege.edu>
2025-02-11 10:39:11 -0600

    Delete M4P4.py

--- M4P4.py
+++
@@ -1,9 +0,0 @@
-# input phase
-firstname = input( "Enter your first name: ")
-steps = int(input( " Enter the number of steps you walked today: "))
-
-# process phase
-calories = steps * .25
-
-# output phase
-print(firstname + ", you burned " + str(calories))
